9b2c83267bb47cdbdb7cf94f8a5524378f5efdaa
er1himanshu <132843132+er1himanshu@users.noreply.github.com>
2025-08-03 08:36:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggle = document.getElementById("mode-toggle");
-    toggle.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
+    const year = document.getElementById("year");
+
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     });
+
+    if (year) {
+        year.textContent = new Date().getFullYear();
+    }
 });
